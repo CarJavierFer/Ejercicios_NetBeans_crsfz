2efd5aa101e2b23f2b0c4333f3231e698a910aee
Carlos Javier Ferrer <153143013+CarJavierFer@users.noreply.github.com>
2024-01-20 10:59:07 +0100

    Update RepetirN.java

--- src/main/java/com/mycompany/ejercicios_netbeans_crsfz/RepetirN.java
+++ src/main/java/com/mycompany/ejercicios_netbeans_crsfz/RepetirN.java
@@ -1,21 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.ejercicios_netbeans_crsfz;
-
 /**
  *
- * @author crsfz (Carlos Javier Ferrer Zapata)
- * Ejercicio de estructura repetitiva
+ * @author crsfz
+ * Este programa en Java genera un número aleatorio n entre 0 y 19,....
+ * imprime el número de iteraciones (que es n + 1), y luego ejecuta un bucle for para generar y mostrar n números aleatorios
  */
 public class RepetirN {
-    
+
     public static void main(String[] args) {
+        
+        //Generación del número aleatorio n:
+        //Se utiliza Math.random() para generar un número decimal aleatorio en el rango [0, 1)
+        //Se multiplica por 20 para obtener un número en el rango [0, 20)
+        //Se realiza un casting a int para convertir el número en un entero y almacenarlo en la variable n.
         int n = (int) (Math.random()*20);
         
+        //Impresión del número de iteraciones:
+        //Imprime en la consola el número de iteraciones, que es n + 1. Se suma 1 porque el rango de iteraciones es [1, n + 1]
         System.out.println("Numero de iteraciones: "+(n+1));
         
+        //Bucle for para generar números aleatorios:
+        //Se utiliza un bucle for que se ejecutará n veces
+        //Dentro del bucle, se genera un número aleatorio x en el rango [18, 64]
+        //Se imprime en la consola el número aleatorio junto con su posición en la iteración (comenzando desde 1)
         for(int i=0; i<n; i++) {
             int x = (int) (Math.random()*(65-8)+18);
             
